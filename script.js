a132ec3f229945fc7f89e7e0faf697481edd2088
gryffonius <gryffonius@gmail.com>
2022-05-11 17:11:30 -0500

    Add vertical sliders to control volume and effect

--- script.js
+++ script.js
@@ -17,3 +17,20 @@ document.addEventListener('DOMContentLoaded', function() {
     c.drawImage(v, 0, 0, w, h);
     setTimeout(updateBacklight, 20, v, c, w, h);
   }
+
+const effectSlider = document.getElementById("effect-slider");
+const volumeSlider = document.getElementById("volume-slider");
+const video = document.getElementById("video");
+const root = document.documentElement;
+
+root.style.setProperty("--effect-slider", "70%")
+
+effectSlider.addEventListener("input", (e) => {
+  root.style.setProperty("--effect-slider", e.target.value + "%")
+})
+
+video.volume = .7
+
+volumeSlider.addEventListener("input", (e) => {
+  video.volume = e.target.value/100
+})
