cecd7797ee89cbbfd34a92062ce31e7dcd4706c9
gryffonius <gryffonius@gmail.com>
2022-05-13 15:01:57 -0500

    Add smaller video file

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var v = document.getElementById('video');
+const effectSlider = document.getElementById("effect-slider");
+const volumeSlider = document.getElementById("volume-slider");
+const video = document.getElementById("video");
+const root = document.documentElement;
+
+const updateBacklight = (v, c, w, h) => {
+  if (v.paused || v.ended) return false;
+  c.drawImage(v, 0, 0, w, h);
+  setTimeout(updateBacklight, 20, v, c, w, h);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
     var canvas = document.getElementById('backlight');
     var context = canvas.getContext('2d');
     var cw = Math.floor(canvas.clientWidth);
     var ch = Math.floor(canvas.clientHeight);
     canvas.width = cw;
     canvas.height = ch;
-    v.addEventListener('play', function() {
+    video.addEventListener('play', function() {
       updateBacklight(this, context, cw, ch);
     }, false);
   }, false);
-  
-  
-  function updateBacklight(v, c, w, h) {
-    if (v.paused || v.ended) return false;
-    c.drawImage(v, 0, 0, w, h);
-    setTimeout(updateBacklight, 20, v, c, w, h);
-  }
-
-const effectSlider = document.getElementById("effect-slider");
-const volumeSlider = document.getElementById("volume-slider");
-const video = document.getElementById("video");
-const root = document.documentElement;
 
 root.style.setProperty("--effect-slider", "70%")
 
